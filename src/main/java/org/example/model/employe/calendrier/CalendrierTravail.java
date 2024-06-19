cf2cd0cc8ba61fd9306c56fcecbee2682e96b359
Jonathan Rajao <hei.tovomihaja@gmail.com>
2024-06-19 16:48:01 +0300

    chore: check if day is public holiday

--- src/main/java/org/example/model/employe/calendrier/CalendrierTravail.java
+++ src/main/java/org/example/model/employe/calendrier/CalendrierTravail.java
@@ -8,27 +8,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalendrierTravail {
-    private  List<LocalDate> mois = new ArrayList<>();
+    private List<LocalDate> mois = new ArrayList<>();
     private List<LocalDate> jour_feries = new ArrayList<>();
 
     public CalendrierTravail(List<LocalDate> mois, List<LocalDate> jour_feries) {
         this.mois = mois;
-        jour_feries.add(LocalDate.of(2024,6,17));
-        jour_feries.add(LocalDate.of(2024,6,25));
-        jour_feries.add(LocalDate.of(2024,6,26));
+        jour_feries.add(LocalDate.of(2024, 6, 17));
+        jour_feries.add(LocalDate.of(2024, 6, 25));
+        jour_feries.add(LocalDate.of(2024, 6, 26));
     }
 
-    public void remplir_mois_de_juin(){
-        LocalDate debut_du_mois = LocalDate.of(2024,6,1);
+    public void remplir_mois_de_juin() {
+        LocalDate debut_du_mois = LocalDate.of(2024, 6, 1);
         LocalDate fin_du_mois = debut_du_mois.withDayOfMonth(debut_du_mois.lengthOfMonth());
-        for(LocalDate date = debut_du_mois ; !date.isAfter(fin_du_mois); date=date.plusDays(1)){
-             mois.add(date);
+        for (LocalDate date = debut_du_mois; !date.isAfter(fin_du_mois); date = date.plusDays(1)) {
+            mois.add(date);
         }
     }
 
 
-    public boolean est_week_end(Jour jour) {
-        DayOfWeek dayOfWeek = jour.getJour().getDayOfWeek();
+    public boolean est_week_end(LocalDate jour) {
+        DayOfWeek dayOfWeek = jour.getDayOfWeek();
         DayOfWeek samedi = DayOfWeek.SATURDAY;
         DayOfWeek dimanche = DayOfWeek.SUNDAY;
         if (dayOfWeek.equals(samedi) || dayOfWeek.equals(dimanche)) {
@@ -37,5 +37,16 @@ public class CalendrierTravail {
         return false;
     }
 
-
+    public boolean est_jour_ferie(LocalDate jour) {
+        for (LocalDate ferie : jour_feries) {
+            if (
+                    ferie.equals(LocalDate.of(2024, 6, 17)) ||
+                            ferie.equals(LocalDate.of(2024, 6, 25)) ||
+                            ferie.equals(LocalDate.of(2024, 6, 17)
+                            )) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
